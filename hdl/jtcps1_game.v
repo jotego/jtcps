de2585feee4d32f855e246e9ed43ffc964436c3b
Jose Tejada <jotego@gmail.com>
2020-01-28 07:30:00 +0100

    wip

---
+++ hdl/jtcps1_game.v
@@ -0,0 +1,323 @@
+/*  This file is part of JTCPS1.
+    JTCPS1 program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    JTCPS1 program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with JTCPS1.  If not, see <http://www.gnu.org/licenses/>.
+
+    Author: Jose Tejada Gomez. Twitter: @topapate
+    Version: 1.0
+    Date: 28-1-2020 */
+`timescale 1ns/1ps
+
+module jtcps1_game(
+    input           rst,
+    input           clk,
+    output          pxl2_cen,   // 12   MHz
+    output          pxl_cen,    //  6   MHz
+    output   [4:0]  red,
+    output   [4:0]  green,
+    output   [4:0]  blue,
+    output          LHBL,
+    output          LVBL,
+    output          LHBL_dly,
+    output          LVBL_dly,
+    output          HS,
+    output          VS,
+    // cabinet I/O
+    input   [ 1:0]  start_button,
+    input   [ 1:0]  coin_input,
+    input   [ 7:0]  joystick1,
+    input   [ 7:0]  joystick2,
+    // SDRAM interface
+    input           downloading,
+    output          dwnld_busy,
+    input           loop_rst,
+    output          sdram_req,
+    output  [21:0]  sdram_addr,
+    input   [31:0]  data_read,
+    input           data_rdy,
+    input           sdram_ack,
+    output          refresh_en,
+    // ROM LOAD
+    input   [21:0]  ioctl_addr,
+    input   [ 7:0]  ioctl_data,
+    input           ioctl_wr,
+    output  [21:0]  prog_addr,
+    output  [ 7:0]  prog_data,
+    output  [ 1:0]  prog_mask,
+    output          prog_we,
+    output          prog_rd,
+    // DIP switches
+    input   [31:0]  status,     // only bits 31:16 are looked at
+    input           dip_pause,
+    input           dip_flip,
+    input           dip_test,
+    input   [ 1:0]  dip_fxlevel, // Not a DIP on the original PCB    
+    // Sound output
+    output  signed [15:0] snd_left,
+    output  signed [15:0] snd_right,
+    output          sample,
+    input           enable_psg,
+    input           enable_fm,
+    // Debug
+    input   [3:0]   gfx_en
+);
+
+wire        snd_cs, main_cs, rom0_cs, rom1_cs, rom2_cs, rom3_cs;
+wire        vram1_cs, vram2_cs, vram3_cs, vram_obj_cs;
+wire [15:1] ram_addr;
+wire [23:1] main_addr;
+wire [15:0] ram_data, main_data;
+wire        ram_ok, main_ok;
+
+wire [ 7:0] dipsw_a, dipsw_b, dipsw_c;
+
+assign snd_cs = 1'b0;
+
+wire [9:0] slot_cs, slot_ok, slot_wr;
+assign slot_cs[0] = main_cs;    // For ROM, RAM and VRAM access
+assign slot_cs[1] = snd_cs;     // For ROM and RAM
+assign slot_cs[2] = rom0_cs;
+assign slot_cs[3] = vram1_cs;
+assign slot_cs[4] = vram2_cs;
+assign slot_cs[5] = vram3_cs;
+assign slot_cs[6] = rom1_cs;
+assign slot_cs[7] = rom2_cs;
+assign slot_cs[8] = rom3_cs;
+assign slot_cs[9] = vram_obj_cs;
+
+assign rom0_ok      = slot_ok[2];
+assign vram1_ok    = slot_ok[3];
+assign vram2_ok    = slot_ok[4];
+assign vram3_ok    = slot_ok[5];
+assign rom1_ok     = slot_ok[6];
+assign rom2_ok     = slot_ok[7];
+assign rom3_ok     = slot_ok[8];
+assign vram_obj_ok = slot_ok[9];
+
+assign slot_wr[9:0] = 9'd0;
+
+wire [31:0] data_write;
+wire        sdram_rnw;
+
+jtcps1_main(
+    .rst        ( rst           ),
+    .clk        ( clk           ),
+    .cen10      ( cen10         ),
+    .cen10b     ( cen10b        ),
+    // Timing
+    input   [8:0]      V,
+    input              LVBL,
+    // PPU
+    output reg         ppu1_cs,
+    output reg         ppu_rstn,
+    // Sound
+    output  reg  [7:0] snd0_latch,
+    output  reg  [7:0] snd1_latch,
+    output             UDSWn,
+    output             LDSWn,
+    // cabinet I/O
+    // Cabinet input
+    .start_button( start_button ),
+    .coin_input  ( coin_input   ),
+    .joystick1   ( joystick1    ),
+    .joystick2   ( joystick2    ),
+    .service     ( 1'b1         ),
+    .tilt        ( 1'b1         ),
+    // BUS sharing
+    input              busreq,
+    output             busack,
+    output             RnW,
+    // RAM access
+    .ram_cs      ( ram_cs       ),
+    .ram_addr    ( ram_addr     ),
+    .ram_data    ( ram_data     ),
+    .ram_ok      ( ram_ok       ),
+    // Video RAM access
+    output  reg        vram_cs,
+    output      [23:1] vram_addr,
+    input       [15:0] vram_data,
+    input              vram_ok,    
+    // ROM access
+    .rom_cs      ( main_cs      ),
+    .rom_addr    ( main_addr    ),
+    .rom_data    ( main_data    ),
+    .rom_ok      ( main_ok      ),
+    // DIP switches
+    .dip_pause   ( dip_pause    ),
+    .dip_test    ( dip_test     ),
+    .dipsw_a     ( dipsw_a      ),
+    .dipsw_b     ( dipsw_b      ),
+    .dipsw_c     ( dipsw_c      )
+);
+
+jtcps1_video UUT (
+    .rst        ( rst           ),
+    .clk        ( clk           ),
+    .pxl_cen    ( cen8          ),
+
+    .hdump      ( hdump         ),
+    .vdump      ( vdump         ),
+    .vrender    ( vrender       ),
+    .frame      ( frame         ),
+
+    // Video signal
+    .HS         ( HS            ),
+    .VS         ( VS            ),
+    .HB         ( HB            ),
+    .VB         ( VB            ),
+    .red        ( red           ),
+    .green      ( green         ),
+    .blue       ( blue          ),
+
+    // Register configuration
+    .hpos1          ( hpos1         ),
+    .vpos1          ( vpos1         ),
+    .hpos2          ( hpos2         ),
+    .vpos2          ( vpos2         ),
+    .hpos3          ( hpos3         ),
+    .vpos3          ( vpos3         ),
+    .vram1_base     ( vram1_base    ),
+    .vram2_base     ( vram2_base    ),
+    .vram3_base     ( vram3_base    ),
+    .vram_obj_base  ( vobj_base     ),
+    .pal_base       ( pal_base      ),
+    // Video RAM interface
+    .vram1_addr     ( vram1_addr    ),
+    .vram1_data     ( vram1_data    ),
+    .vram1_ok       ( vram1_ok      ),
+    .vram1_cs       ( vram1_cs      ),
+
+    .vram2_addr     ( vram2_addr    ),
+    .vram2_data     ( vram2_data    ),
+    .vram2_ok       ( vram2_ok      ),
+    .vram2_cs       ( vram2_cs      ),
+
+    .vram3_addr     ( vram3_addr    ),
+    .vram3_data     ( vram3_data    ),
+    .vram3_ok       ( vram3_ok      ),
+    .vram3_cs       ( vram3_cs      ),
+
+    .vram_obj_addr  ( vram_obj_addr ),
+    .vram_obj_data  ( vram_obj_data ),
+    .vram_obj_ok    ( vram_obj_ok   ),
+    .vram_obj_cs    ( vram_obj_cs   ),
+
+    // GFX ROM interface
+    .rom1_addr  ( rom1_addr     ),
+    .rom1_bank  ( rom1_bank     ),
+    .rom1_half  ( rom1_half     ),
+    .rom1_data  ( rom1_data     ),
+    .rom1_cs    ( rom1_cs       ),
+    .rom1_ok    ( rom1_ok       ),
+
+    .rom2_addr  ( rom2_addr     ),
+    .rom2_bank  ( rom2_bank     ),
+    .rom2_half  ( rom2_half     ),
+    .rom2_data  ( rom2_data     ),
+    .rom2_cs    ( rom2_cs       ),
+    .rom2_ok    ( rom2_ok       ),
+
+    .rom3_addr  ( rom3_addr     ),
+    .rom3_bank  ( rom3_bank     ),
+    .rom3_half  ( rom3_half     ),
+    .rom3_data  ( rom3_data     ),
+    .rom3_cs    ( rom3_cs       ),
+    .rom3_ok    ( rom3_ok       ),
+
+    .rom0_addr  ( rom0_addr      ),
+    .rom0_bank  ( rom0_bank      ),
+    .rom0_half  ( rom0_half      ),
+    .rom0_data  ( rom0_data      ),
+    .rom0_cs    ( rom0_cs        ),
+    .rom0_ok    ( rom0_ok        )
+);
+
+jtframe_sdram_mux #(
+    // VRAM read access:
+    .SLOT3_AW   ( 18    ),
+    .SLOT4_AW   ( 18    ),  //4
+    .SLOT5_AW   ( 18    ),  //5
+    .SLOT9_AW   ( 18    ),  // OBJ VRAM
+
+    .SLOT3_DW   ( 16    ),
+    .SLOT4_DW   ( 16    ),
+    .SLOT5_DW   ( 16    ),
+    .SLOT9_DW   ( 16    ),
+    // GFX ROM
+    .SLOT2_AW   ( 22    ),  // OBJ VRAM
+    .SLOT6_AW   ( 22    ),  //6
+    .SLOT7_AW   ( 22    ),  //7
+    .SLOT8_AW   ( 22    ),  //8
+
+    .SLOT2_DW   ( 32    ),
+    .SLOT6_DW   ( 32    ),
+    .SLOT7_DW   ( 32    ),
+    .SLOT8_DW   ( 32    )
+)
+u_sdram_mux(
+    .rst            ( rst           ),
+    .clk            ( clk           ),
+    .vblank         ( 1'b0          ),
+
+    // VRAM read access only
+    .slot9_offset   ( vram_offset       ),
+    .slot9_addr     ( vram_obj_addr[18:1]  ),
+    .slot9_dout     ( vram_obj_data     ),
+
+    .slot3_offset   ( vram_offset       ),
+    .slot3_addr     ( vram1_addr[18:1]  ),
+    .slot3_dout     ( vram1_data        ),
+
+    .slot4_offset   ( vram_offset       ),
+    .slot4_addr     ( vram2_addr[18:1]  ),
+    .slot4_dout     ( vram2_data        ),
+
+    .slot5_offset   ( vram_offset       ),
+    .slot5_addr     ( vram3_addr[18:1]  ),
+    .slot5_dout     ( vram3_data        ),
+
+    // GFX ROM
+    .slot2_offset   ( gfx_offset/*^(22'b01_111<<17) */       ),
+    .slot2_addr     ( gfx0_addr         ),
+    .slot2_dout     ( rom0_data         ),
+
+    .slot6_offset   ( gfx_offset        ),
+    .slot6_addr     ( gfx1_addr         ),
+    .slot6_dout     ( rom1_data         ),
+
+    .slot7_offset   ( gfx_offset        ),
+    .slot7_addr     ( gfx2_addr         ),
+    .slot7_dout     ( rom2_data         ),
+
+    .slot8_offset   ( gfx_offset        ),
+    .slot8_addr     ( gfx3_addr         ),
+    .slot8_dout     ( rom3_data         ),
+
+    // bus signals
+    .slot_cs        ( slot_cs       ),
+    .slot_ok        ( slot_ok       ),
+    .slot_wr        ( slot_wr       ),
+
+    // SDRAM controller interface
+    .downloading    ( downloading   ),
+    .loop_rst       ( loop_rst      ),
+    .sdram_ack      ( sdram_ack     ),
+    .sdram_req      ( sdram_req     ),
+    .refresh_en     ( refresh_en    ),
+    .sdram_addr     ( sdram_addr    ),
+    .sdram_rnw      ( sdram_rnw     ),
+    .data_rdy       ( data_rdy      ),
+    .data_read      ( data_read     ),   
+    .data_write     ( data_write    )
+);
+
+endmodule
