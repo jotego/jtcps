5f7800a1df2d68b49c8a971ab4e3cdf34c6db802
jotego <jotego@gmail.com>
2020-02-26 07:21:00 +0100

    minor

--- ver/game/go.sh
+++ ver/game/go.sh
@@ -67,6 +67,7 @@ echo "Game ROM length: " $GAME_ROM_LEN
 ../../modules/jtframe/bin/sim.sh -mist -d GAME_ROM_LEN=$GAME_ROM_LEN \
     -sysname cps1 -modules ../../modules -d SCANDOUBLER_DISABLE=1 \
     -d COLORW=8 -d STEREO_GAME=1 -d JTFRAME_WRITEBACK=1 \
+    -d BUTTONS=4 \
     -d SCAN2X_TYPE=5 -d JT51_NODEBUG -d CPSB_CONFIG="$CPSB_CONFIG" \
     -videow 384 -videoh 224 \
-    -video $OTHER
+    $OTHER -video 
