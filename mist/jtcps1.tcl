4dfae114ee228aa1ece7538443e0fa0cd285bd2f
Jose Tejada <jotego@gmail.com>
2020-03-12 08:52:10 +0100

    Fast load support on MiST

--- mist/jtcps1.tcl
+++ mist/jtcps1.tcl
@@ -1,4 +1,3 @@
 set_global_assignment -name VERILOG_MACRO "MISTTOP=jtcps1_mist"
 set_global_assignment -name VERILOG_MACRO "SCAN2X_TYPE=2"
 set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=384"
-set_global_assignment -name VERILOG_MACRO "JTFRAME_MIST_DIRECT=0"
