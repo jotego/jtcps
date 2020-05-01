98680cf5d2b7f164641da57b4cd2ac9eaa4b465a
Jose Tejada <jotego@gmail.com>
2020-05-01 19:06:28 +0200

    minor

--- bin/jtupdate.sh
+++ bin/jtupdate.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-parallel jtcore cps1 {} -d JTFRAME_OSD_NOLOAD \
-                        -d JTFRAME_RELEASE \
-                        -d JTFRAME_OSD_NOSND \
-                        -ftp-folder CPS $* \
-                        ::: -mist -mister -sidi
+jtseed cps1 -d JTFRAME_OSD_NOLOAD \
+            -d JTFRAME_RELEASE \
+            -d JTFRAME_OSD_NOSND \
+            -ftp-folder CPS $* \
