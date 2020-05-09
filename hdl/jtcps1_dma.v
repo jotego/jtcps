365911c30af8f2f2b13ba0f4effaf7b3ca692443
Jose Tejada <jotego@gmail.com>
2020-05-09 17:28:38 +0200

    Captain Commando continue text shown

--- hdl/jtcps1_dma.v
+++ hdl/jtcps1_dma.v
@@ -127,7 +127,7 @@ wire        tile_vs  = vrender1==9'd12; // Vertical start, use for SCR3
 // various addresses
 wire [17:1] vrow_addr = { vram_row_base[9:1], 8'd0 } + 
                         { 7'd0, row_offset[9:0] } + {1'b0,vrenderf },
-            vscr_addr = { vram_scr_base, 8'd0 } + { 4'd0, scan, scr_cnt[0] },
+            vscr_addr = { vram_scr_base[8:5], 5'd0, 8'd0 } + { 4'd0, scan, scr_cnt[0] },
             vobj_addr = { vram_obj_base[9:1], 8'd0 } + { 7'd0, obj_cnt[9:2], obj_cnt[1:0] },
             vpal_addr = { vram_pal_base[9:1], 8'd0 } + { 5'd0, pal_rd_page , pal_cnt };
 
--- hdl/jtcps1_mmr.v
+++ hdl/jtcps1_mmr.v
@@ -228,17 +228,6 @@ always@(posedge clk) begin
     end
 end
 
-function [15:0] data_sel;
-    input [15:0] olddata;
-    input [15:0] newdata;
-    input [ 1:0] dsn;
-    `ifndef VIDEOSIMULATION
-    data_sel = { dsn[1] ? olddata[15:8] : newdata[15:8], dsn[0] ? olddata[7:0] : newdata[7:0] };
-    `else
-    data_sel = olddata;
-    `endif
-endfunction
-
 wire reg_rst;
 reg  pre_copy, pre_dma_ok;
 
@@ -356,34 +345,34 @@ always @(posedge clk, posedge reg_rst) begin
             obj_dma_ok <= pre_dma_ok;
             pre_dma_ok <= 1'b0;
         end
-        if( ppu1_cs ) begin
+        if( ppu1_cs && dsn==2'b00 ) begin // only full 16-bit writes are allowed
             case( addr[5:1] )
                 // CPS-A registers
                 5'h00: begin
-                    vram_obj_base <= data_sel(vram_obj_base , cpu_dout, dsn);
+                    vram_obj_base <= cpu_dout;
                     pre_dma_ok   <= 1'b1;
                 end
-                5'h01: vram1_base    <= data_sel(vram1_base    , cpu_dout, dsn);
-                5'h02: vram2_base    <= data_sel(vram2_base    , cpu_dout, dsn);
-                5'h03: vram3_base    <= data_sel(vram3_base    , cpu_dout, dsn);
-                5'h04: vram_row_base <= data_sel(vram_row_base , cpu_dout, dsn);
+                5'h01: vram1_base    <= cpu_dout;
+                5'h02: vram2_base    <= cpu_dout;
+                5'h03: vram3_base    <= cpu_dout;
+                5'h04: vram_row_base <= cpu_dout;
                 5'h05: begin
-                    pal_base      <= data_sel(pal_base      , cpu_dout, dsn);
+                    pal_base      <= cpu_dout;
                     pre_copy      <= 1'b1;
                     //$display("PALETTE!");
                 end
-                5'h06: hpos1      <= data_sel(hpos1     , cpu_dout, dsn);
-                5'h07: vpos1      <= data_sel(vpos1     , cpu_dout, dsn);
-                5'h08: hpos2      <= data_sel(hpos2     , cpu_dout, dsn);
-                5'h09: vpos2      <= data_sel(vpos2     , cpu_dout, dsn);
-                5'h0a: hpos3      <= data_sel(hpos3     , cpu_dout, dsn);
-                5'h0b: vpos3      <= data_sel(vpos3     , cpu_dout, dsn);
-                5'h0c: hstar1     <= data_sel(hstar1    , cpu_dout, dsn);
-                5'h0d: vstar1     <= data_sel(vstar1    , cpu_dout, dsn);
-                5'h0e: hstar2     <= data_sel(hstar2    , cpu_dout, dsn);
-                5'h0f: vstar2     <= data_sel(vstar2    , cpu_dout, dsn);
-                5'h10: row_offset <= data_sel(row_offset, cpu_dout, dsn);
-                5'h11: ppu_ctrl   <= data_sel(ppu_ctrl  , cpu_dout, dsn);
+                5'h06: hpos1      <= cpu_dout;
+                5'h07: vpos1      <= cpu_dout;
+                5'h08: hpos2      <= cpu_dout;
+                5'h09: vpos2      <= cpu_dout;
+                5'h0a: hpos3      <= cpu_dout;
+                5'h0b: vpos3      <= cpu_dout;
+                5'h0c: hstar1     <= cpu_dout;
+                5'h0d: vstar1     <= cpu_dout;
+                5'h0e: hstar2     <= cpu_dout;
+                5'h0f: vstar2     <= cpu_dout;
+                5'h10: row_offset <= cpu_dout;
+                5'h11: ppu_ctrl   <= cpu_dout;
             endcase
         end
         if( ppu2_cs ) begin
