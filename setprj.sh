6106aef810a0acafaf1d1ab835936bf7ec03a457
Jose Tejada <jotego@gmail.com>
2020-09-15 19:10:08 +0200

    Moved to multi core folder structure

--- setprj.sh
+++ setprj.sh
@@ -3,8 +3,9 @@
 source modules/jtframe/bin/setprj.sh --quiet
 alias jtcore="$JTFRAME/bin/jtcore cps1 -ftp-folder CPS"
 # derived variables
-VER=$JTROOT/ver
-GAME=$VER/game
-VIDEO=$VER/video
-HDL=$JTROOT/hdl
+CORE=cps1
+VER=$JTROOT/cores/$CORE/ver
+GAME=$VER/cores/$CORE/game
+VIDEO=$VER/cores/$CORE/video
+HDL=$JTROOT/cores/$CORE/hdl
 OKI=$MODULES/jt6295
