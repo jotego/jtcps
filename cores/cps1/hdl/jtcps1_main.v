8d22af831841055376f1a4d23a388937a4073da2
Jose Tejada <jotego@gmail.com>
2020-10-18 20:17:00 +0200

    WOF now boots up without hanging up

--- cores/cps1/hdl/jtcps1_main.v
+++ cores/cps1/hdl/jtcps1_main.v
@@ -160,13 +160,14 @@ always @(posedge clk, posedge rst) begin
             dbus_cs     <= ~|A[23:18]; // all must be zero
             pre_vram_cs <= A[23:18] == 6'b1001_00 && A[17:16]!=2'b11;
             io_cs       <= A[23:20] == 4'b1000;
-            rom_cs      <= A[23:22] == 2'b00;
             pre_ram_cs  <= &A[23:18];
             `ifdef CPS15
             io15_cs      <= A[23:16] == 8'hf1 && A[15:14]==2'b11;
             main2qs_cs   <= A[23:16]==8'hf1 && A[15] && (A[14]==A[13]); // F18000~F19FFF F1E000~F1FFFF
             main2qs_addr <= A;
+            rom_cs      <= A[23:21] == 3'b00;
             `else
+            rom_cs      <= A[23:22] == 2'b00;
             `endif
             if( io_cs ) begin // PAL IOA1 (16P8B @ 12F)
                 ppu1_cs  <= A[8:6] == 3'b100; // 'h10x
