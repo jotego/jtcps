ca99651bcf5e5ac4b4da991e83e99406dea8e67a
jotego <jotego@gmail.com>
2020-10-22 08:48:55 +0200

    wip

--- cores/cps15/hdl/jtcps15_sound.v
+++ cores/cps15/hdl/jtcps15_sound.v
@@ -104,6 +104,10 @@ always @(posedge clk, posedge rst) begin
 end
 `endif
 
+`ifdef SIMULATION
+wire bank_access = rom_cs & A[15];
+`endif
+
 assign      ram_we    = ram_cs && !wr_n;
 assign      main_we   = !main_busakn && !z80_buswn && (main_addr[16:13]==4'hc || main_addr[16:13]==4'hf);
 
--- cores/cps15/ver/game/mist_dump.v
+++ cores/cps15/ver/game/mist_dump.v
@@ -69,13 +69,13 @@ module mist_dump(
             //$shm_probe(UUT.u_frame.u_board.u_sdram, "A");
             `ifndef NOMAIN
             $shm_probe(UUT.u_game.u_main, "A");
-            //$shm_probe(UUT.u_game.u_sound, "A");
+            $shm_probe(UUT.u_game.u_sound, "A");
             //$shm_probe(UUT.u_game.u_sound.cpu2dsp);
-            $shm_probe(UUT.u_game.u_eeprom,"A");
+            //$shm_probe(UUT.u_game.u_eeprom,"A");
             //$shm_probe(UUT.u_game.u_sound.u_buslock, "A");
             `endif
             //$shm_probe(UUT.u_game.u_video, "A");
-            $shm_probe(UUT.u_game.u_video.u_colmix, "A");
+            //$shm_probe(UUT.u_game.u_video.u_colmix, "A");
 
             /*
             $shm_probe(UUT.u_game.u_video.u_dma.step );
