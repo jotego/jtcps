00c21fe622a22f940db30e6f63c22059abee09e8
jotego <jotego@gmail.com>
2021-01-10 18:04:46 +0100

    temp

--- cores/cps1/hdl/jtcps1_dma.v
+++ cores/cps1/hdl/jtcps1_dma.v
@@ -133,14 +133,14 @@ wire        tile_vs  = vrender1 == (flip ? 9'd17 : 9'd12); // Vertical start, us
 // various addresses: note that the base address is added, and not just
 // concatenated. This can be seen in Ghouls start where the palette base
 // address is 9105, the LSB cannot be discarded
-// Although I have only seen evidence for the palette, I am treating all
-// base address operations as additive
-wire [17:1] //vrow_addr = { vram_row_base[9:3], row_offset[9:0] + vrenderf[9:0] },
-            vrow_addr = { vram_row_base[9:0], 7'd0 } + { 7'd0, row_offset[9:0] + vrenderf[9:0] },
-            //vscr_addr = { vram_scr_base[8:5], scan, scr_cnt[0] },
-            vscr_addr = { vram_scr_base[8:0], 8'd0 } + { 4'd0, scan, scr_cnt[0] },
-            // vobj_addr = { vram_obj_base[9:3], obj_cnt },
-            vobj_addr = { vram_obj_base[9:0], 7'd0  } + { 7'd0, obj_cnt },
+// Although I have only seen evidence for the palette. If vscr_addr is treated
+// in the same way, Slam Masters background breaks down completely
+wire [17:1] vrow_addr = { vram_row_base[9:3], row_offset[9:0] + vrenderf[9:0] },
+            // vrow_addr = { vram_row_base[9:0], 7'd0 } + { 7'd0, row_offset[9:0] + vrenderf[9:0] },
+            vscr_addr = { vram_scr_base[8:5], scan, scr_cnt[0] },
+            // vscr_addr = { vram_scr_base[8:0], 8'd0 } + { 4'd0, scan, scr_cnt[0] },
+            vobj_addr = { vram_obj_base[9:3], obj_cnt },
+            // vobj_addr = { vram_obj_base[9:0], 7'd0  } + { 7'd0, obj_cnt },
             //vpal_addr = { vram_pal_base[9:5], pal_rd_page , pal_cnt };
             vpal_addr = { vram_pal_base[9:0], 7'd0 } + { 5'd0, pal_rd_page , pal_cnt };
 
