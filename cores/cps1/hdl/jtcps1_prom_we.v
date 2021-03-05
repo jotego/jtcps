54c780658bf93b17eca6e36cf603a8db101e10a4
Jose Tejada <jotego@gmail.com>
2021-03-05 09:58:18 +0100

    64MB wip

--- cores/cps1/hdl/jtcps1_prom_we.v
+++ cores/cps1/hdl/jtcps1_prom_we.v
@@ -146,7 +146,7 @@ always @(posedge clk) begin
         prog_addr <= is_cpu ? bulk_addr[23:1] + CPU_OFFSET : (
                      is_snd ?  snd_addr[23:1] + SND_OFFSET : (
                      is_oki ?  pcm_addr[23:1] + PCM_OFFSET :
-                     is_gfx ?  gfx_addr[22:1] + GFX_OFFSET : {9'd0, bulk_addr[12:0]}));
+                     is_gfx ?  {gfx_addr[24],gfx_addr[22:1]} + GFX_OFFSET : {10'd0, bulk_addr[12:0]}));
         prog_ba   <= (is_cpu||is_snd) ? 2'd0 : ( is_gfx ? gfx_bank : 2'd1 );
         if( is_kabuki )
             kabuki_sr <= 2'b11;
