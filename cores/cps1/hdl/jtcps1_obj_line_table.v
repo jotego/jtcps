jotego/jtcps

commit f7a9dba7854c4c828fb0ed9c4a18318f7ca36a19

Fixes CSCLUB intro glitch

cores/cps1/hdl/jtcps1_obj_line_table.v
@@ -100,8 +100,8 @@ jtcps1_obj_tile_match u_tile_match(
 
     .vflip      ( vflip     ),
     .vrenderf   ( vrenderf  ),
-    .obj_y      ( obj_y[8:0]),
-
+    .obj_y      ( obj_y[9:0]), // it was 8:0, changed to 9:0 to match CPS2. Watch out for
+                               // possible new issues
     .vsub       ( vsub      ),
     .inzone     ( inzone    ),
     .code_mn    ( code_mn   )

log/builds.log
@@ -1634,3 +1634,4 @@ cps2          	mister	20210303	1	FAIL	6c8f596	0da91b2	FX68K=026efe4	JT51=64546fa
 cps2          	mister	20210303	5019	PASS	af9f0c6	0da91b2	FX68K=026efe4	JT51=64546fa	JT6295=87e6f47	JTDSP16=7ab28bd	JTEEPROM=51358ae
 cps2          	mister	20210303	1	PASS	a60299d	0da91b2	FX68K=026efe4	JT51=64546fa	JT6295=87e6f47	JTDSP16=7ab28bd	JTEEPROM=51358ae
 cps2          	mister	20210303	1	FAIL	a60299d	0da91b2	FX68K=026efe4	JT51=64546fa	JT6295=87e6f47	JTDSP16=7ab28bd	JTEEPROM=51358ae
+cps2          	mist	20210304	1	PASS	0e5845d	6737c07	FX68K=026efe4	JT51=64546fa	JT6295=87e6f47	JTDSP16=7ab28bd	JTEEPROM=51358ae

cores/cps1/hdl/jtcps1_obj_match.v
@@ -15,22 +15,22 @@
     Author: Jose Tejada Gomez. Twitter: @topapate
     Version: 1.0
     Date: 13-1-2020 */
-    
+
 
 module jtcps1_obj_match #(parameter [3:0] OFFSET=0)(
     input            clk,
     input      [3:0] tile_m,
     input      [8:0] vrender,
-    input      [8:0] obj_y,
+    input      [9:0] obj_y,
     output reg       match
 );
 
-reg  [8:0]  vfinal, vfinal2;
+reg  [9:0]  vfinal, vfinal2;
 reg         below, inzone;
 
 always @(*) begin
-    vfinal = obj_y  + {1'b0,OFFSET,4'd0};
-    vfinal2= vfinal + 9'h10;
+    vfinal = obj_y  + {2'b0,OFFSET,4'd0};
+    vfinal2= vfinal + 10'h10;
     below  = vrender >= vfinal;
     inzone = vrender < vfinal2;
 end

cores/cps1/hdl/jtcps1_obj_tile_match.v
@@ -27,7 +27,7 @@ module jtcps1_obj_tile_match(
 
     input             vflip,
     input      [ 8:0] vrenderf,
-    input      [ 8:0] obj_y,
+    input      [ 9:0] obj_y,
 
     output reg [ 3:0] vsub,
     output reg        inzone,
@@ -52,7 +52,7 @@ endgenerate
 
 always @(*) begin
     inzone = match!=16'd0;
-    vsub   = vrenderf-obj_y;
+    vsub   = vrenderf-obj_y[8:0];
     vsub   = vsub ^ {4{vflip}};
     // which m won?
     case( match )

cores/cps2/hdl/jtcps2_obj_scan.v
@@ -46,7 +46,7 @@ module jtcps2_obj_scan(
 reg  [ 9:0] mapper_in;
 reg  [ 8:0] vrenderf;
 
-wire [ 8:0] obj_y;
+wire [ 9:0] obj_y;
 wire [15:0] code_mn;
 wire [ 9:0] eff_x;
 wire [ 1:0] obj_bank;
@@ -78,7 +78,7 @@ jtcps1_obj_tile_match u_tile_match(
 );
 
 assign      prio       = table_x[15:13];
-assign      obj_y      = table_y[8:0];
+assign      obj_y      = table_y[9:0];
 assign      obj_bank   = table_y[14:13];
 assign      tile_m     = table_attr[15:12];
 assign      tile_n     = table_attr[11: 8];

cores/cps2/hdl/jtcps2_objram.v
@@ -60,18 +60,17 @@ module jtcps2_objram(
 parameter AW=13; // 13 for full table shadowing
 localparam [3:1] XOFF=4, YOFF=5;
 
-reg  [ 8:0] off_y;
-reg  [ 9:0] off_x;
+reg  [ 9:0] off_x, off_y;
 reg  [15:0] din_x, din_y;
 
 wire [   1:0] wex, wey, wecode, weattr;
 wire [AW-3:0] wr_addr, gfx_addr;
 
-wire [8:0] next_offy = { cfg_dsn[1] ? off_y[8]   : cpu_dout[8],
-                         cfg_dsn[0] ? off_y[7:0] : cpu_dout[7:0] } - 9'h10;
+wire [9:0] next_offy = { cfg_dsn[1] ? off_y[9:8] : cpu_dout[9:8],
+                         cfg_dsn[0] ? off_y[7:0] : cpu_dout[7:0] } - 10'h10;
 
 wire [9:0] next_offx = { cfg_dsn[1] ? off_x[9:8] : cpu_dout[9:8],
-                         cfg_dsn[0] ? off_x[7:0] : cpu_dout[7:0] } - 9'h40;
+                         cfg_dsn[0] ? off_x[7:0] : cpu_dout[7:0] } - 10'h40;
 
 assign wex      = ~dsn & {2{cs & main_addr[2:1]==2'd0}};
 assign wey      = ~dsn & {2{cs & main_addr[2:1]==2'd1}};
@@ -83,7 +82,7 @@ assign gfx_addr = { ~obank, obj_addr[AW-4:0] };
 
 always @(posedge clk_cpu) begin
     ok    <= cs;
-    din_y <= { oram_din[15:9], oram_din[8:0] - off_y };
+    din_y <= { oram_din[15:10], oram_din[9:0] - off_y };
     din_x <= { oram_din[15:10], oram_din[9:0] - off_x };
 end
 

commit 6f3562b0f5072389a9dbe3341e1ca7ad9fc65a18

Fixes broken wall in Dynasty Wars introduced in commit f7a9dba7

cores/cps1/hdl/jtcps1_obj_line_table.v
@@ -63,6 +63,7 @@ wire [15:0] match;
 reg  [ 2:0] wait_cycle;
 reg         last_tile;
 wire [ 3:0] offset, mask;
+wire [ 9:0] ext_y;
 wire        unmapped;
 
 assign      tile_m     = obj_attr[15:12];
@@ -71,6 +72,11 @@ assign      vflip      = obj_attr[6];
 wire        hflip      = obj_attr[5];
 //          pal        = obj_attr[4:0];
 assign      eff_x      = obj_x + { 1'b0, npos, 4'd0}; // effective x value for multi tile objects
+`ifdef CPS2
+    assign  ext_y      = obj_y[9:0];
+`else
+    assign  ext_y      = { obj_y[8], obj_y[8:0]}
+`endif
 
 wire [15:0] code_mn;
 reg  [ 4:0] st;
@@ -100,8 +106,7 @@ jtcps1_obj_tile_match u_tile_match(
 
     .vflip      ( vflip     ),
     .vrenderf   ( vrenderf  ),
-    .obj_y      ( obj_y[9:0]), // it was 8:0, changed to 9:0 to match CPS2. Watch out for
-                               // possible new issues
+    .obj_y      ( ext_y     )
     .vsub       ( vsub      ),
     .inzone     ( inzone    ),
     .code_mn    ( code_mn   )

log/builds.log
@@ -1687,3 +1687,5 @@ cps1          	mister	20210306	1	FAIL	9deb98d	476fb7e	FX68K=026efe4	JT51=75a7057
 cps1          	mister	20210306	1	PASS	9deb98d	476fb7e	FX68K=026efe4	JT51=2b7417c	JT6295=87e6f47	JTDSP16=7ab28bd	JTEEPROM=51358ae
 cps1          	mister	20210307	1	FAIL	9deb98d	476fb7e	FX68K=026efe4	JT51=2b7417c	JT6295=87e6f47	JTDSP16=7ab28bd	JTEEPROM=51358ae
 cps2          	mister	20210307	2301	PASS	418d9bf	476fb7e	FX68K=026efe4	JT51=2b7417c	JT6295=87e6f47	JTDSP16=7ab28bd	JTEEPROM=51358ae
+cps1          	mister	20210307	1	FAIL	e4d34f3	476fb7e	FX68K=026efe4	JT51=2b7417c	JT6295=87e6f47	JTDSP16=7ab28bd	JTEEPROM=51358ae
+cps1          	mister	20210307	1	PASS	e4d34f3	476fb7e	FX68K=026efe4	JT51=2b7417c	JT6295=87e6f47	JTDSP16=7ab28bd	JTEEPROM=51358ae