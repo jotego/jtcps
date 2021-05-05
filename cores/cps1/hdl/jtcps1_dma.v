660a26fe42d7d50849a81ac6620aae051da0ec0c
Jose Tejada <jotego@gmail.com>
2021-05-05 17:11:17 +0200

    Fixes random horizontal line glitches

--- cores/cps1/hdl/jtcps1_dma.v
+++ cores/cps1/hdl/jtcps1_dma.v
@@ -183,9 +183,10 @@ end
 
 reg [15:0] tile_din, obj_din, pal_din;
 
-reg last_br;
-always @(posedge clk) last_br <= br;
-assign vram_clr = last_br & ~br;
+//reg last_br;
+//always @(posedge clk) last_br <= br;
+//assign vram_clr = last_br & ~br;
+assign vram_clr = 0;
 
 // Tile cache
 jtframe_dual_ram #(.dw(16), .aw(9)) u_tile_cache(
