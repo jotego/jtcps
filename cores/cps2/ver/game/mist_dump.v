18e1e37e3c75f28144adea6562fb74ea5225d607
jotego <jotego@gmail.com>
2021-05-06 08:46:19 +0200

    minor

--- cores/cps2/ver/game/mist_dump.v
+++ cores/cps2/ver/game/mist_dump.v
@@ -89,6 +89,7 @@ module mist_dump(
                 $shm_probe(UUT.u_game.u_main, "A");
                 $shm_probe(u_harness.u_sdram, "A");
             `endif
+            $shm_probe(UUT.u_game.u_video.u_obj.u_scan,"A");
             //$shm_probe(UUT.u_game.u_main.u_cpu.excUnit.regs68L);
             //$shm_probe(UUT.u_game.u_main.u_cpu.excUnit.regs68H);
             //$shm_probe(UUT.u_game.u_sdram, "A");
