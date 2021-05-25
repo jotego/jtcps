71dea3c3585fce60a9880b832becf53ae629540e
Jose Tejada <jotego@gmail.com>
2021-05-25 12:58:04 +0200

    Disables fast write!

--- cores/cps1/hdl/jtcps1_sdram.v
+++ cores/cps1/hdl/jtcps1_sdram.v
@@ -259,7 +259,7 @@ jtframe_ram_5slots #(
     .SDRAMW      ( 23            ),
     .SLOT0_AW    ( 17            ), // Main CPU RAM
     .SLOT0_DW    ( 16            ),
-    .SLOT0_FASTWR(  1            ),
+    .SLOT0_FASTWR(  0            ),
 
     .SLOT1_AW    ( 17            ), // VRAM - read only access
     .SLOT1_DW    ( 16            ),
@@ -274,7 +274,7 @@ jtframe_ram_5slots #(
     .SLOT3_AW    ( 21            ), // Main CPU ROM
     .SLOT3_DW    ( 16            ),
     .SLOT3_LATCH (  1            ),
-    .SLOT3_DOUBLE(  0            ),
+    .SLOT3_DOUBLE(  1            ),
 
     .SLOT4_AW    ( Z80_AW        ), // Sound CPU
     .SLOT4_DW    (  8            )
